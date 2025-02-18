842f096c876d87364f802c065228f68ad1f9290f
JAGADEESH A LATTI <117442431+jagadeeshlatti@users.noreply.github.com>
2025-02-18 14:23:29 +0530

    Jaggu here

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Login from './LoginSignup'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <Login />
+      </div>
+      <div>
+        <h1></h1>
+      </div>
+    </>
+  )
+}
+
+export default App
